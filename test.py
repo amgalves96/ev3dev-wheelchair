fadb766b4bcd7cd6a8664b575ab86713531f4ff9
Afonso Alves <amg.alves@campus.fct.unl.pt>
2022-05-22 19:28:45 +0100

    upd

--- test.py
+++ test.py
@@ -2,10 +2,11 @@
 
 from time import sleep
 
-from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank, SpeedRPM
+from ev3dev2.motor import LargeMotor, OUTPUT_A, OUTPUT_B, SpeedPercent, MoveTank, SpeedRPM, MoveSteering
 from ev3dev2.sensor import INPUT_1
 from ev3dev2.sensor.lego import TouchSensor
 from ev3dev2.led import Leds
+from ev3dev.core import RemoteControl
 
 import os
 import sys
@@ -13,9 +14,10 @@ import time
 
 #test
 
-m = LargeMotor(OUTPUT_A)
-tank_drive = MoveTank(OUTPUT_A, OUTPUT_B)
-ts = TouchSensor()
+#m = LargeMotor(OUTPUT_A)
+drive = MoveSteering(OUTPUT_A, OUTPUT_B)
+#ts = TouchSensor()
+rc = RemoteControl()
 
 def debug_print(*args, **kwargs):
     '''Print debug messages to stderr.
@@ -30,12 +32,18 @@ debug_print('Code uploaded!')
 print("You can see this!")
 
 while True:
-    if ts.is_pressed:
+    if rc.red_up:
         #m.on_for_seconds(SpeedRPM(150), 5)
-        tank_drive.on_for_rotations(SpeedPercent(75), SpeedPercent(75), 10)
+        drive.on(0, 20)
         # drive in a different turn for 3 seconds
         #tank_drive.on_for_seconds(SpeedPercent(60), SpeedPercent(30), 3)
-    #else:
-     #   m.stop()
+    elif rc.red_down:
+        drive.on(0, -20)
+    elif rc.blue_up:
+        drive.on(-100, 20)
+    elif rc.blue_down:
+        drive.on(100, 20)
+    else:
+        drive.on(0, 0)
     # don't let this loop use 100% CPU
-    sleep(0.01)
+    #sleep(0.01)
